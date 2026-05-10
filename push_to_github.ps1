eef55cd4b0209d0d0aaf32528b544fa2b220e4c1
Rajan Kumar Sharma <sharmarajankumar822@gmail.com>
2026-05-11 02:50:05 +0530

    Remove obsolete scripts

--- push_to_github.ps1
+++
@@ -1,6 +0,0 @@
-git rm -r --cached .
-git add .
-git commit -m "Initial commit: Generalize project and update README"
-git branch -M main
-git remote add origin https://github.com/uds822/social_media_agent.git
-git push -u origin main -f
